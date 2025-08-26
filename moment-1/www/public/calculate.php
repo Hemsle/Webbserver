f4cc870f42ed393d1ccca160f26e4967a264d578
Carl Hemsle <carl@hemsle.se>
2025-08-26 11:45:13 +0200

    calc

---
+++ moment-1/www/public/calculate.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Kalkylation</title>
+</head>
+<h1>Kalkylering</h1>
+<body>
+    <?php
+    $nmr1 = $_POST['n1'];
+    $nmr2 = $_POST['n2'];
+    $sum = $nmr1 + $nmr2;
+
+    echo "<p>Summan av kardemumman blir $sum </p>"
+    ?>
+</body>
+</html>
--- moment-1/www/public/person.php
+++ moment-1/www/public/person.php
@@ -1,16 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Person</title>
 </head>
+
 <body>
     <h1>Namn och ålder</h1>
     <?php
     $name = $_POST['name'];
     $age = $_POST['age'];
+    
+    echo "<p>Du heter $name och är $age år gammal <br> </p>";
 
-    echo "<p>Du heter $name och är $age år gammal </p>"
-    ?>  
+    if($age < 65) {
+        $left = 65 - $age;
+        echo "<p> Du har $left år kvar till pension</p>";
+    } else {
+        echo "<p> Du är pensionerad </p>";
+    }
+    ?>
 </body>
+
 </html>
