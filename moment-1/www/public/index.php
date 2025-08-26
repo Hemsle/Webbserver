8cf4f84611c34560af8912bb1d18409df20f1427
Carl Hemsle <carl@hemsle.se>
2025-08-26 10:47:09 +0200

    first commit

---
+++ moment-1/www/public/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <title>Valuta</title>
+</head>
+<body>
+<h1>Mata in US$</h1>
+<form action="currency.php" method="post">
+<form>
+    <label>Ange ett belopp i US$</label>
+    <input type="number" name="currency">
+    <input type="submit" value="Konvertera till sek">
+</form>
+</body>
+</html>
