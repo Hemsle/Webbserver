bfa3869de59c577310302814229ea09ced583f7b
Carl Hemsle <carl@hemsle.se>
2025-08-27 09:54:28 +0200

    moment-0kopia

---
+++ moment-0(kopia)/www/public/evaluate.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head lang="sv"></head>
+<body>
+	<?php include 'eval.html'; ?>
+<?php
+     	$ans1 = $_POST['q1'];
+     	$ans2 = $_POST['q2'];
+		$ans3 = $_POST['q3'];
+     	$ans4 = $_POST['q4'];
+		$ans5 = $_POST['q5'];
+
+        $name = $_POST['name'];
+     	$points = 0;
+     	
+     	if($ans1 == 10)
+           $points++;
+     	if($ans2 == 15)
+           $points++;
+		if($ans3 == 50)
+           $points++;
+     	if($ans4 == 3)
+           $points++;
+		if($ans5 == 4)
+           $points++;
+     	
+     	echo("<p>" . $name . " fick " . $points . " av 5 möjliga</p>");
+?>
+	<input type="text">
+</body>
+</html>
---
+++ moment-0(kopia)/www/public/index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head lang="sv"></head>
+<body>
+<h1>Matematik-test-Carl</h1>
+<form action="evaluate.php" method="post">
+   <fieldset>
+       <legend>Frågor</legend>
+       <label>5 + 5 = </label>
+       <input type="number" name="q1">
+       <br>
+       <label>3 x 5 = </label>
+       <input type="number" name="q2">
+       <br>
+       <label>10 x 5 = </label>
+       <input type="number" name="q3">
+       <br>
+       <label>12 / 4 = </label>
+       <input type="number" name="q4">
+       <br>
+       <label>2 + 2 = </label>
+       <input type="number" name="q5">
+       <br>
+       <label>Namn</label>
+       <input type="text" name="name">
+       <br>
+       <input type="submit" value="Rätta">
+   </fieldset>
+</form>
+</body>
+</html>
