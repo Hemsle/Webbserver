999e27886c8988cf90a8886d0a7713eef24baee6
Carl Hemsle <carl@hemsle.se>
2025-09-30 11:20:16 +0200

    moment 3

---
+++ moment-3/moment3/www/public/pages/start.php
@@ -0,0 +1,7 @@
+<h1>Välkommen till Webbserverprogrammering 1</h1>
+<h2>Moment 2</h2>
+<p>Vi kommer använda include() för att länka in sidor i vår mall.</p>
+
+<p>
+    antal besökare: <?php include("inc/count.php"); ?>
+</p>
